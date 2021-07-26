9e6b8a08c2447af0c6aea4a0c1b8a68698e836c7
JaneshAgrawal <85166020+JaneshAgrawal@users.noreply.github.com>
2021-07-26 19:06:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# crossyRoad4
+# angryBirdsStage1
+Stage 1 Angry Birds
--- sketch.js
+++ sketch.js
@@ -1,101 +1,56 @@
-var grid = 50;
-var width = 1366;
-var carGroup1,logGroup1;
-var grassHeight = 100;
-var gameState = "play";
-var carAnimation1,carAnimation2, logAnimation, playerAnimation,playerImage;
-var school;
-var city,cityAnimation;
-var gameState = "play";
-function preload()
-{carAnimation1=loadAnimation("images/car1.png");
-carAnimation2=loadAnimation("images/car2.png");
-playerAnimation=loadAnimation("images/Player-03.png");
-logAnimation=loadAnimation("images/log2.png");
- cityAnimation=loadAnimation("images/city1.png");
-}
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-function setup() {
-  createCanvas(1366,700);
-  carGroup1 = new Group();
-  logGroup1 = new Group();
-  city=createSprite(width/2,-1500);
-  city.addAnimation("city",cityAnimation);
-  for (var i=0;i<6;i++){
-    var bottomGrass1 = createSprite(683,height-50-(i*400),width,grassHeight);
+var engine, world;
+var stone, iron,rubber,hammer;
+var sand1, sand2, sand3, sand4,sand5,sand6;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
     
-    if(i%2===0){
-      var road= createSprite(683,height-150-(i*400)-grassHeight,width,300,)
-      road.shapeColor="black";
-    }
-    bottomGrass1.shapeColor="grey";
-  }
-  for(var i = 0; i<40; i++){
-    cars = new Car(2);
-    carGroup1.add(cars.spt);
-  }
- for(var i = 0; i<40; i++){
-   log=new Log(-2);
-   logGroup1.add(log.spt);
- }
- player=new Player(width/2,height-25);  
- }
- 
-function draw() {
-  background("skyblue");
-  translate(0,-player.spt.y+height-150);
-  for(i=1;i<logGroup1.length;i++){
- if(logGroup1[i].x<0)
-   logGroup1[i].x=width;
- }
- for(i=1;i<carGroup1.length;i++){
-   if(carGroup1[i].x<0)
-   {
+    stone = new Stone(700,320,100,100);
+    iron = new Iron(300,350);
+    rubber=new Rubber(900,450,70);
+    hammer = new Hammer(10,100);
 
-   
-   carGroup1[i].x=width;
- }
-if(carGroup1[i].x>width){
-  carGroup1[i].x=0;
-}
- }
- if(carGroup1.isTouching(player.spt)){
-   player.spt.x = width/2;
-   player.spt.y = height-75;
- }
- if(logGroup1.isTouching(player.spt)){
-   player.spt.x = player.spt.x-3;
- }
-else if(( player.spt.y > height-1550 && player.spt.y < height-1300) ||
+    sand1 = new Sand(505,450,10);
+    sand2 = new Sand(510,450,10);
+    sand3 = new Sand(515,450,10);
+    sand4 = new Sand(520,450,10);
+    sand5 = new Sand(525,450,10);
+    sand6 = new Sand(515,445,10);
+    sand7 = new Sand(520,445,10);
+    sand8 = new Sand(525,445,10);
+    sand9 = new Sand(530,445,10);
+    sand10 = new Sand(535,445,10);
 
- (player.spt.y < height-500 && player.spt.y > height-850) ||
- (player.spt.y>height) ||
- (player.spt.x<0) ||
- (player.spt.x>width)){
 
-  player.spt.x=width/2;
-  player.spt.y = height-75;}
-  if(city.isTouching(player.spt)){
-    gameState = "Win";
-  }
-if(gameState==="Win") {
-  stroke("Green");
-  fill("Green");
-  textSize(40);
-  text("Congratulations! You Made It",width/2-250,-1700);
- carGroup1.destroyEach();
- logGroup1.destroyEach();
 }
-  drawSprites();
-}
-function keyPressed(){
-  if(keyCode == UP_ARROW){
-    player.move(0,-2);
-  }else if(keyCode == DOWN_ARROW){
-    player.move(0,2);
-  }else if(keyCode == LEFT_ARROW){
-    player.move(-2,0);
-  }else if(keyCode == RIGHT_ARROW){
-    player.move(2,0);
-  }
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+    stone.display();
+    plane.display();
+    iron.display();
+    rubber.display();
+
+    hammer.display();
+
+    sand1.display();
+    sand2.display();
+    sand3.display();
+    sand4.display();
+    sand5.display();
+    sand6.display();
+    sand7.display();
+    sand8.display();
+    sand9.display();
+    sand10.display();
 }
